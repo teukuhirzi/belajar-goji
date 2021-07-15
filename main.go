b4c7287f1863e6c6c653bc7b0a505b2e2beaa3ab
teukuhirzi <lowfinecity@gmail.com>
2021-07-15 10:08:17 +0700

    first commit

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	//"goji.io"
+	//"goji.io/pat"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
+func hello(w http.ResponseWriter, r *http.Request) {
+	//name := pat.Param(r, "name")
+	//fmt.Fprintf(w, "Hello, %s!", name)
+	fmt.Println("Hello World")
+}
 
-	http.ListenAndServe(":8080", nil)
+func main() {
+	//mux := goji.NewMux()
+	//mux.HandleFunc(pat.Get("/hello/:name"), hello)
+
+	//http.ListenAndServe("localhost:8000", mux)
 }
